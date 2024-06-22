7843f1cd91069e0c935089fdde96634f22e2f236
Miqueias-cyber <edmar.miqueias@discente.ufma.br>
2024-06-22 13:22:24 -0300

    adição de pastas e modificação  da tela
    add duas pastas para dividir o código em partes menores

--- jogo.py
+++ jogo.py
@@ -1,11 +1,13 @@
 import pygame
+import constantes #pasta onde estarão as variaveis
+import sprites #imagens do jogo 
 from pygame.locals import *
 from sys import exit
 
-pygame.init()
+pygame.init() 
 
-largura = 640
-altura = 480
+largura = 840
+altura = 680
 
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption('Jogo')
@@ -15,7 +17,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(tela, (255,0,0), (200,300,40,50))  
-    pygame.draw.circle(tela, (0,255,0), (300,250), 40)  
-    pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)    
+    #pygame.draw.rect(tela, (255,0,0), (200,300,40,50))  
+    #pygame.draw.circle(tela, (0,255,0), (300,250), 40)  
+    #pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)    
     pygame.display.update()        
