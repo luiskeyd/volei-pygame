007b08b7dd0d1f3115a59d4f44d9918af287bf22
Miqueias-cyber <edmar.miqueias@discente.ufma.br>
2024-06-23 16:27:29 -0300

    add de sprites
    add um dos jogadores e fiz a animação dele caso alguma tecla seja pressionada

--- jogo.py
+++ jogo.py
@@ -8,16 +8,67 @@ pygame.init()
 
 largura = 840
 altura = 680
+relogio=pygame.time.Clock()
+
+#Cores
+PRETO=(0,0,0)
+BRANCO=(255,255,255)
 
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption('Jogo Vôlei ')
+pygame.display.set_caption('Sprites')
+
+class Jogador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('imagens/New Piskel.png'))
+        self.sprites.append(pygame.image.load('imagens/New Piskel (1).png'))
+        self.sprites.append(pygame.image.load('imagens/New Piskel (2).png'))
+        self.atual=0
+        self.image = self.sprites[self.atual]
+        self.image = pygame.transform.scale(self.image,(32*4,32*4))
+
+        self.rect=self.image.get_rect()
+        self.rect.topleft=100, 100
+
+        self.animar=False
+
+    def update(self):
+        if self.animar==True:
+            self.atual = self.atual + 0.2
+            if self.atual>= len(self.sprites):
+                self.atual = 0
+                self.animar=False
+            self.image= self.sprites[int(self.atual)]
+            self.image = pygame.transform.scale(self.image,(32*4,32*4))
+    
+    def andar(self):
+        self.animar=True
+
+
+todas_as_sprites= pygame.sprite.Group()
+jogador1 = Jogador()
+todas_as_sprites.add(jogador1)
+
+
+
+
 
 while True:
+    relogio.tick(40)
+    tela.fill(BRANCO)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+        if event.type==KEYDOWN:
+            jogador1.andar
+
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+
     #pygame.draw.rect(tela, (255,0,0), (200,300,40,50))  
     #pygame.draw.circle(tela, (0,255,0), (300,250), 40)  
     #pygame.draw.line(tela, (255,255,0), (390,0), (390,600), 5)    
-    pygame.display.update()        
+    pygame.display.flip()        
